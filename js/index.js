c841f0afc8e6ee3a4f9108a621af83e2727693fb
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2025-01-10 03:19:47 +0200

    feat: add services section responsive

--- js/index.js
+++ js/index.js
@@ -4,9 +4,17 @@ window.addEventListener('scroll', function () {
     // Get the about section element
     var aboutSection = document.querySelector('#about');
     var aboutTitle = document.querySelector('#about h2');
+    var servicesSection = document.querySelector('#services');
+    var servicesTitle = document.querySelectorAll('#services p');
     // Check if the about section is in the viewport
     if (aboutSection.getBoundingClientRect().top <= window.innerHeight / 2) {
         // Add the animate__bounce class to the about section
         aboutTitle.classList.add('animate__bounce');
+    }
+    if (servicesSection.getBoundingClientRect().top <= window.innerHeight / 2) {
+        // Add the animate__bounceInLeft class to each p in the services section
+        servicesTitle.forEach(function (title) {
+            title.classList.add('animate__pulse');
+        });
     }
 });
--- ts/index.ts
+++ ts/index.ts
@@ -3,10 +3,18 @@ window.addEventListener('scroll', function () {
     // Get the about section element
     const aboutSection = document.querySelector('#about') as HTMLDivElement;
     const aboutTitle = document.querySelector('#about h2') as HTMLElement;
+    const servicesSection = document.querySelector('#services') as HTMLDivElement;
+    const servicesTitle = document.querySelectorAll('#services p') as NodeListOf<HTMLElement>;
 
     // Check if the about section is in the viewport
     if (aboutSection.getBoundingClientRect().top <= window.innerHeight / 2) {
         // Add the animate__bounce class to the about section
         aboutTitle.classList.add('animate__bounce');
+    }
+    if (servicesSection.getBoundingClientRect().top <= window.innerHeight / 2) {
+        // Add the animate__bounceInLeft class to each p in the services section
+        servicesTitle.forEach(function (title) {
+            title.classList.add('animate__pulse');
+        });
     }
 });
